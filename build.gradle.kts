1e0d212ec4caf48188c69fc75d64218dff94d512
antonlosk <antonloskutovqwerty@gmail.com>
2026-02-09 16:58:53 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
-// build.gradle.kts (в корне)
+// build.gradle.kts (в корне проекта)
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "9.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
 }
