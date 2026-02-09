24a7f247fb671d3d6ebf53ffc35b0990adcb9b1b
antonlosk <antonloskutovqwerty@gmail.com>
2026-02-09 18:45:05 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
-// build.gradle.kts (в корне проекта)
+// build.gradle.kts (Root)
 plugins {
+    // Android Gradle Plugin 9.0.0
     id("com.android.application") version "9.0.0" apply false
+    
+    // Kotlin 2.3.10
     id("org.jetbrains.kotlin.android") version "2.3.10" apply false
+    
+    // !!! ВАЖНО: Новый плагин компилятора Compose (нужен для Kotlin 2.0+)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
 }
